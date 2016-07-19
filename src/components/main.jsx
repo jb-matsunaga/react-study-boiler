7c80eb1941f0be0d5e16745205cece5495fe7171
jb-matsunaga <jb.matsunaga@gmail.com>
2016-07-20 01:17:30 +0900

    見出しとテキストのコンポーネントを３つ連続で描画する

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,14 +1,44 @@
 import React from 'react'
 
+var TextBox = React.createClass({
+    render() {
+        console.log(this.props.data);
+        return(
+            <div className="c-txtBox">
+                <h1>{this.props.data.title}</h1>
+                <p>{this.props.data.text}</p>
+            </div>
+        )
+    }
+
+});
+
 module.exports = React.createClass({
     getInitialState() {
-      return {
-        text: 'Hello world!'
-      }
+        return {
+            'box': [
+                {
+                    title: 'title1',
+                    text: 'text1text1text1text1text1text1text1'
+                },
+                {
+                    title: 'title2',
+                    text: 'text2text2text2text2text2text2text2'
+                },
+                {
+                    title: 'title3title3title3title3title3title3',
+                    text: 'text3text3text3text3text3text3text3text3text3text3text3text3text3text3'
+                }
+            ]
+        }
     },
     render() {
-      return ( 
-        <h1>{this.state.text}</h1>
-      )  
+        return (
+            <div>
+                <TextBox data={this.state.box[0]}/>
+                <TextBox data={this.state.box[1]}/>
+                <TextBox data={this.state.box[2]}/>
+            </div>
+        )
     }
 });
